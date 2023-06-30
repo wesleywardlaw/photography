bb53b7270ef60f4936e42569a828fa324daeeb81
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2023-06-30 11:41:05 -0400

    nonsense route

--- pages/api/login.js
+++ pages/api/login.js
@@ -4,8 +4,12 @@ import passport from '../../lib/passport';
 
 const handler = nextConnect();
 
-handler.use(auth).post(passport.authenticate('local'), (req, res) => {
-    res.json({ user: req.user });
+// handler.use(auth).post(passport.authenticate('local'), (req, res) => {
+//     res.json({ user: req.user });
+// });
+
+handler.get((req, res) => {
+    res.status(200).json({ hello: 'hi' });
 });
 
 export default handler;
