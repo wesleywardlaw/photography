1221032c8241d18038d1f34626c875ae21c7220a
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2023-06-30 15:51:20 -0400

    add head

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import Navbar from '../components/Navbar';
 import '../styles.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import Head from 'next/head';
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
             <QueryClientProvider client={queryClient}>
                 <Navbar />
                 <main>
