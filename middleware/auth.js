011ee81b29d7a34c974a4b483c38a8465d618030
Wesley Wardlaw <wesley.ryan.wardlaw@gmail.com>
2025-02-01 11:04:45 -0600

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -16,6 +16,11 @@ const auth = nextConnect()
             },
         })
     )
+    .use((req, res, next) => {
+        console.log('Session ID:', req.sessionID);
+        console.log('Session expires:', req.session.cookie.expires);
+        next();
+      })
     .use(passport.initialize())
     .use(passport.session());
 
